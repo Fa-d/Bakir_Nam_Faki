1816dc7fb29eebccef75bda88644df16226ec55a
fahad <fsadakathussain@gmail.com>
2023-06-20 18:38:44 +0600

    init commit

---
+++ practices/3/RyzenVPN/app/src/main/java/com/faddy/ryzenvpn/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.faddy.ryzenvpn.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.faddy.ryzenvpn.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
